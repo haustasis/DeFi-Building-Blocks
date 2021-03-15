2bc76fa9c3bf57064383a93c0d6ea76ad845f8cb
TARISO <toni.ariso@gmail.com>
2021-03-15 07:03:10 +0100

    flashloan

---
+++ liquidity-mining/contracts/UnderlyingToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.7.3;
+
+
+import '@openzeppelin/contracts/ERC20/ERC20.sol';
+
+contract UnderlyingToken is ERC20 {
+  constructor() ERC20('Lp Token', 'LTK') {}
+
+  function faucet(address to, uint amount) external {
+    _mint(to, amount);
+  }
+}
+
